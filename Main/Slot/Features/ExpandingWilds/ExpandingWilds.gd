424d78c528fe1543712816ccb35dd6b30322c915
Stefano <stefano@elysiumstudios.se>
2022-10-06 18:31:38 +0300

    Bugfix

--- Main/Slot/Features/ExpandingWilds/ExpandingWilds.gd
+++ Main/Slot/Features/ExpandingWilds/ExpandingWilds.gd
@@ -1,5 +1,5 @@
 extends Node
-
+export(Vector2) var offset;
 var animationPlayer : AnimationPlayer;
 var fightfx : Node2D;
 
@@ -10,4 +10,4 @@ func _ready():
 
 #{data:{from:4, positions:[0, 1, 2, 3, 4, 5, 6, 7, 8], tileid:10}, id:0, type:ExpandingWild}
 func set_pos(x,y):
-	$RobotFightFx.global_position = Globals.singletons["Slot"].get_tile_global_default_position(x, y);
+	$RobotFightFx.global_position = Globals.singletons["Slot"].get_tile_global_default_position(x, y, true)+offset;
